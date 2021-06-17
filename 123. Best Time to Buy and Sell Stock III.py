f6d47f11fc737a31e3119cd3b06734bfe38a6ce3
NiramBtw <80960403+NiramBtw@users.noreply.github.com>
2021-06-17 16:59:11 +0300

    Create 123. Best Time to Buy and Sell Stock III.py

---
+++ 123. Best Time to Buy and Sell Stock III.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        prices = [3,3,5,0,0,3,1,4]
+        state - rest, hold, sell
+        * can to only 2 transactions
+        start at 0 no stock
+        the stock price is postive 
+        """
+        
+        # states
+        a = -prices[0]
+        b = float('-inf')
+        c = float('-inf')
+        d = float('-inf')
+        
+        for price in prices:
+            a = max(a, -price)
+            b = max(b, a + price)
+            c = max(c, b -price)
+            d = max(d, c + price)
+        return d
